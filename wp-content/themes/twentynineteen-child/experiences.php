be8a812337f515f2160a043d4c8ca4880435186d
jolipixel <morgan@jolipixel.fr>
2019-09-03 19:18:14 +0700

    update slug

--- wp-content/themes/twentynineteen-child/experiences.php
+++ wp-content/themes/twentynineteen-child/experiences.php
@@ -45,7 +45,7 @@ get_header(); ?>
                     </a>
                 <?php endwhile; ?>
                 <?php wp_reset_query(); ?>
-                <a href="<?php home_url('consulting'); ?>" class="btn-view-all"><i class="fas fa-arrow-right"></i> <?= __('Back to the Consulting page', DOMAIN); ?></a>
+                <a href="<?= home_url('consulting'); ?>" class="btn-view-all"><i class="fas fa-arrow-right"></i> <?= __('Back to the Consulting page', DOMAIN); ?></a>
             </div>
             <?php endif; ?>
         </article>
